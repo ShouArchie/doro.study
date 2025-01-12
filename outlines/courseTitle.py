f0fb5b56251403e2acbee413c8bb31478632b354
Allen Lian <jyallen06@gmail.com>
2025-01-11 22:43:41 -0500

    Added Course Descriptions

--- outlines/courseTitle.py
+++ outlines/courseTitle.py
@@ -20,24 +20,49 @@ def extract_course_name(file_path):
         
         return "Course name not found"
 
+def extract_course_description(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            html_content = file.read()
+            cd_index1 = html_content.find("Course Description")
+            cd_index2 = html_content.find("Course Description", cd_index1 + 18)
+            cd_start = html_content.find("<p>", cd_index2 + 18) + 3
+            cd_end = html_content.find("</p>", cd_index2 + 18)
+            return html_content[cd_start:cd_end]
+       
+        return "Course description not found in the HTML file."
+    
+    except Exception as e:
+        return f"An error occurred: {e}"
+
 main_folder_path = "outlines\\SimplifiedHTMLFiles"
 
-names = []
+# names = []
+
+# for folder in os.listdir(main_folder_path):
+#     folder_path = os.path.join(main_folder_path, folder)
+#     for file in os.listdir(folder_path):
+#         file_path = os.path.join(folder_path, file)
+#         course_name = extract_course_name(file_path)
+#         names.append(course_name)
+
+descriptions = []
 
 for folder in os.listdir(main_folder_path):
     folder_path = os.path.join(main_folder_path, folder)
     for file in os.listdir(folder_path):
         file_path = os.path.join(folder_path, file)
-        course_name = extract_course_name(file_path)
-        names.append(course_name)
+        course_description = extract_course_description(file_path)
+        descriptions.append(course_description)
+# print(descriptions)
 
-output_csv = "outlines\\master_outline.csv"
-input_csv = "outlines\\combined_output.csv"
+output_csv = "outlines\\master_outline2.csv"
+input_csv = "outlines\\master_outline.csv"
 df = pd.read_csv(input_csv)
 csv_data = df.values.tolist()
 
 with open(output_csv, "w", newline="") as outfile:
     writer = csv.writer(outfile)
-    writer.writerow(["Code", "Personnel", "Schemes", "Course Title"])
-    for i in range(len(names)):
-        writer.writerow([csv_data[i][0], csv_data[i][1], csv_data[i][2], names[i]])
+    writer.writerow(["code", "personnel", "schemes", "course_title", "course_description"])
+    for i in range(len(descriptions)):
+        writer.writerow([csv_data[i][0], csv_data[i][1], csv_data[i][2], csv_data[i][2], descriptions[i]])
--- outlines/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-result = []
-# flag = False
-# for i in csv_data:
-#     if flag == False:
-#         flag == True
-#         continue
-#     result.append(csv_data[i].append(names[i]))
